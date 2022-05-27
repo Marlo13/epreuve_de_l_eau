2c73d99e27a259dfe5f63bca2e7a2a06685999a7
Lauris <laurisbessonepro@gmail.com>
2022-05-27 16:46:52 +0200

    deuxieme push

--- eau02.rb
+++ eau02.rb
@@ -10,19 +10,33 @@ def enver_a_l(arr)
 end
 enver_a_l(ARGV)
 =end
+=begin
+def enver(arr)
+   n = arr.length
+        while n != -1
+        a1 = arr.rotate(n)
+            n = n -1
+        end
+
+   
+end
+=end
+
+
 def reversee(arr)
     a = 0
-    z = arr.length
+    z = arr.length - 1
+    
     while a < z
     
-    
-    arr[a] switch avec arr[z]
-    
-    
-    
-    
+    arr[a], arr[z] = arr[z], arr[a]
+  
     a = a + 1
     z = z - 1
     end
 
+puts arr
+
 end
+
+reversee(ARGV)
