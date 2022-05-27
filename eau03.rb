d09e9b40f2a4a41fd8d963433a8d43840572102d
Lauris <laurisbessonepro@gmail.com>
2022-05-27 18:03:43 +0200

    exercice fini

---
+++ eau03.rb
@@ -0,0 +1,34 @@
+#crée une liste Fibonacci
+
+#pour crée la liste faut que les deux element d avant s additionne pour donner la suite
+
+#faut que quand ARGV = a arr.length sa puts le dernier
+def plus_que_un(arr)
+    if arr[0].to_i < 0
+        puts "-1"
+    elsif arr[0] == nil
+        puts "-1" 
+    elsif !arr.all?(/\d/)#gere les string
+        puts "-1"
+    else
+        fibonacci_suit(arr)
+    end
+end
+
+def fibonacci_suit(arr)
+    n = ARGV[0].to_i
+    arr = [0, 1]
+    a = 0
+    b = 1
+
+    while arr.length < n
+        c = arr[a] + arr[b]
+        arr << c
+        
+        b = b + 1
+        a = a + 1
+    end
+    print arr.last
+end
+
+plus_que_un(ARGV)
