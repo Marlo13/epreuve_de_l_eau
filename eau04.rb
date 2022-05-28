361da5515fa92ed251eb1a39532007eb675fde95
Lauris <laurisbessonepro@gmail.com>
2022-05-28 14:16:52 +0200

    Premier push

---
+++ eau04.rb
@@ -0,0 +1,26 @@
+#nombre premier divisible que par lui meme et 1
+
+
+def number_first(arr)
+    n = ARGV[0].to_i
+    arr = []
+    a = 0
+    while a > n# trouver une condition qui s arrete apres etre superieur de 1 fois
+        count=0
+        for i in 1..a+1 do
+             d=a%i
+            if d==0 then
+                count+=1
+            end
+        end
+          
+        if count==2 then
+            arr << a
+        else
+        end
+        a = a+1
+    end
+    puts arr.last
+end
+
+number_first(ARGV[0])
