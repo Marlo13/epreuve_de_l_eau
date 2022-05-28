7d8865519ad99ee8f32ecab60ad69616189ab2ca
Lauris <laurisbessonepro@gmail.com>
2022-05-28 15:44:43 +0200

    consigne

---
+++ eau05.rb
@@ -0,0 +1,15 @@
+=begin
+Créez un programme qui détermine si une chaîne de caractère se trouve dans une autre.
+
+Exemples d’utilisation :
+$> python exo.py bonjour jour
+true
+
+$> python exo.py bonjour joure
+false
+
+$> python exo.py 42
+error
+
+Afficher error et quitter le programme en cas de problèmes d’arguments.
+=end
