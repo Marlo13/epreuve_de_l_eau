557fef89ec6a50d39beaa35dd98e946125c38f50
Lauris <laurisbessonepro@gmail.com>
2022-05-28 14:53:43 +0200

    deuxieme push

--- eau04.rb
+++ eau04.rb
@@ -5,19 +5,29 @@ def number_first(arr)
     n = ARGV[0].to_i
     arr = []
     a = 0
-    while a > n# trouver une condition qui s arrete apres etre superieur de 1 fois
+    b = 0
+    
+    while b == 0 # trouver une condition qui s arrete apres etre superieur de 1 fois        
+        
         count=0
+
+        
         for i in 1..a+1 do
              d=a%i
             if d==0 then
                 count+=1
             end
         end
-          
         if count==2 then
             arr << a
         else
         end
+
+        if a > n 
+            b = b + 1
+        else
+        end
+
         a = a+1
     end
     puts arr.last
