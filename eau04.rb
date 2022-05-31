4b3535d5bd5d8fed662a4d57d22859f2c89e178e
Lauris <laurisbessonepro@gmail.com>
2022-05-31 16:54:26 +0200

    exercice fini

--- eau04.rb
+++ eau04.rb
@@ -6,11 +6,8 @@ def number_first(arr)
     a = 0
     b = 0
     
-    while b == 0 # trouver une condition qui s arrete apres etre superieur de 1 fois        
-        
+    while b == 0 # trouver une condition qui s arrete apres etre superieur de 1 fois                
         count=0
-
-        
         for i in 1..a+1 do
              d=a%i
             if d==0 then
@@ -26,7 +23,6 @@ def number_first(arr)
             b = b + 1
         else
         end
-
         a = a+1
     end
    return arr.last
--- test.rb
+++ test.rb
@@ -1,33 +1,136 @@
+=begin
+Créez un programme qui détermine si une chaîne de caractère se trouve dans une autre.
 
+Exemples d’utilisation :
+$> python exo.py bonjour jour
+true
 
-def menu 
-    puts '1. Convertir des radians en degrés.'
-    puts '2. Convertir des degrés en radians.'
-    puts '3. Convertir des kilomètres par heure en mètres par seconde.'
-    puts '4. Convertir des mètres par seconde en kilomètres par heure.'
-    puts '5. Quitter.'
-    choice = 0
-    while choice < 1 or choice > 5
-      print "\nVotre choix : "
-      choice = gets.chomp.to_i
-    end
-    return choice
-  end
-  
-  PI = 3.14159265359
-  DEG_TO_RAD = 180 / PI
-  MS_TO_KMH = 3.6
-  choice = menu
-  if choice != 5
-    print "\nEntrez le number : "
-    number = gets.chomp.to_f
-    if choice == 1
-      print number * DEG_TO_RAD
-    elsif choice == 2
-      print number / DEG_TO_RAD
-    elsif choice == 3
-      print number / MS_TO_KMH
+$> python exo.py bonjour joure
+false
+
+$> python exo.py 42
+error
+
+Afficher error et quitter le programme en cas de problèmes d’arguments.
+=end
+=begin
+def caractere(arr)
+    if arr[0].include?(arr[1])
+        puts true
     else
-      print number * MS_TO_KMH
+        puts false
     end
-  end
+end
+caractere(ARGV)
+
+
+def caractere_Deux(arr)
+    if arr[0].match(arr[1])
+        puts true
+    else 
+        puts false
+    end
+end
+caractere_Deux(ARGV)
+=end
+
+# prendre le mot1 comparer lettre par lettre la premiere du mot2 
+#iterer pour le mot1 si exact passer a la deuxieme lettre du mot2 
+# à partir de la lettre du mot1 qui == a la lettre mot2
+
+mot1 = ARGV[0].split('')
+mot2 = ARGV[1].split('')
+
+def strin_contains(mot1, mot2)
+    i = 0
+    j = 0
+    
+    while i < mot1.size # si i n est pas egal a j = i + 1 sounon
+        #puts i, mot1[i], 'iii'
+        print mot1[i], mot2[j], '...' #si j prends +1 alors arreter si apres il n est plus egal a i
+        if j > 0
+            if mot1[i] == mot2[j]
+            else 
+                puts false
+                exit
+            end
+        end
+
+        if mot1[i] == mot2[j] # si i == j 
+            if j == mot2.size-1 #si j == la longueur de mot2
+                if mot2[j] == mot1[i]# si j == i
+                    puts true
+                    exit
+                
+                elsif mot1[i] != mot2[j]# sinon si i != j
+                    puts false
+                    exit
+                end
+                
+            end
+            i += 1
+            j += 1
+        elsif i == mot1.size
+            
+            if mot1[i] != mot2[j]
+                puts false
+                exit
+            end
+        elsif j == mot2.size-1
+            puts false
+            exit
+        else
+            i += 1
+        end
+
+    end
+    
+end
+
+strin_contains(mot1, mot2)
+
+
+gerer quand mot1[i] = mot2[j] doit increment i et j 
+if mot1[i] = mot2[j]
+    i +=1
+    j +=1
+end
+
+gerer quand mot1[i] != mot2[j] doit incrementif i
+if mot1[i] != mot2[j]
+    i +=1
+end
+
+
+gerer quand mot1[i] = mot2[j] on deja etait egal 
+if j > 0
+    if mot1[i] == mot2[j]
+    else
+        puts false
+        exit
+    end
+end
+
+
+gerer quand mot1[i] est finie / si mot2[j] est finie ou pas/ si fini tout les deux derniere lettre ==?
+gerer quand mot2[j] est finie / si mot1[i] est finie ou pas/ si fini tout les deux derniere lettre ==?
+if i == mot1.size
+    if j == mot2.size
+        puts les deux fini
+    else
+        puts false
+        exit
+    end
+elsif j == mot2.size
+    if i == mot1.size
+            puts les deux fini
+    else
+            puts false
+            exit
+    end
+else
+end
+
+
+gerer quand dans mot1[i] il y a plusieur fois la meme lettre qui peut etre dans mot2[j]/ commment commencer a la bonne lettre ? du coup si une autre egaliter 
+et que la second lettre pas bonne aller a la suivante?
