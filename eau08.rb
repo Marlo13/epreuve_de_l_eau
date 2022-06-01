b73af99aad560edaac5e59ef37554ffa3535cd77
Lauris <laurisbessonepro@gmail.com>
2022-06-01 16:48:43 +0200

    exercice fini

--- eau08.rb
+++ eau08.rb
@@ -15,3 +15,26 @@ false
 
 Afficher error et quitter le programme en cas de problèmes d’arguments.
 =end
+
+def numeric?(arr)
+    a = 0
+    while a < arr.size
+        p arr[a]
+        if arr[a].match(/\w/)
+            puts "false"
+            exit
+        else
+        end
+        a += 1 
+    end
+    return true
+end
+
+#gestion d erreur
+(puts "error" ; exit) if ARGV[0] == nil
+#parsing
+table = ARGV
+#resolution
+result = numeric?(table)
+#affichage
+puts result
